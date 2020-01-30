f48996d197d6c8357611216c2a80546a97bec51e
kashyapjhaveri <33370217+kashyapjhaveri@users.noreply.github.com>
2020-01-30 12:07:10 -0500

    Defined member variables for person class and their getter and setters

--- src/main/java/Group2_W2020_MAD3463_FP/Person.java
+++ src/main/java/Group2_W2020_MAD3463_FP/Person.java
@@ -1,6 +1,99 @@
 package Group2_W2020_MAD3463_FP;
 
+import java.time.LocalDate;
+
 abstract public class Person implements IDisplay {
     //Common member Variables
     /*Common member methods*/
+
+    private Integer id;
+    private String firstName,lastName;
+    private Gender gender;
+    private LocalDate birthDate;
+    private Integer age;
+    private String mobileNumber;
+    private String email;
+    private String userName;
+    private String password;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
