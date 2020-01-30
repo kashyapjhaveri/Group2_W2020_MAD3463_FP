681bbf91878bf827af49411fdf55e49f84b89248
kashyapjhaveri <33370217+kashyapjhaveri@users.noreply.github.com>
2020-01-30 17:21:46 -0500

    Declared driver class member variables and their getter and setter

--- src/main/java/Group2_W2020_MAD3463_FP/Driver.java
+++ src/main/java/Group2_W2020_MAD3463_FP/Driver.java
@@ -1,8 +1,34 @@
 package Group2_W2020_MAD3463_FP;
 
 public class Driver extends Person {
+
+    private String drivingLicenceNumber;
+    private Boolean isHistroyCleared;
+    private double salary;
+
+    public String getDrivingLicenceNumber() {
+        return drivingLicenceNumber;
+    }
+
+
+    public Boolean getHistroyCleared() {
+        return isHistroyCleared;
+    }
+
+    public void setHistroyCleared(Boolean histroyCleared) {
+        isHistroyCleared = histroyCleared;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     @Override
     public void print() {
-
     }
 }
+
